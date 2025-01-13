d6497d38c8ca2041813ab86ae370225782bbaa3d
Pranto Deb <rajprit0m@outlook.com>
2025-01-14 01:10:55 +0600

    spread and rest operator

---
+++ module1/src/1.7.ts
@@ -0,0 +1,26 @@
+{
+  // spread operator
+  // rest operator
+  // destructuring
+
+  // spread operator
+  const user1: string[] = ["a", "b", "c", "d", "e"];
+  const user2: string[] = ["f", "g", "h", "i"];
+  user1.push(...user2);
+
+  const mentors1 = {
+    cse: "indra",
+    eee: "saki",
+  };
+  const mentors2 = {
+    mech: "raja",
+    chem: "dip",
+  };
+  const mentorList = { ...mentors1, ...mentors2 };
+
+  // rest operator
+  const greetFriends = (...friends: string[]) => {
+    friends.forEach((friend: string) => console.log(`Hi ${friend}`));
+  };
+  greetFriends("Abul", "Mabul", "Tabul");
+}
