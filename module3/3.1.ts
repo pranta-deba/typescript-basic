1ccfc618a9d22f5964860afbd2931a2fa782b35d
Pranto Deb <rajprit0m@outlook.com>
2025-01-16 20:23:14 +0600

    Class and object

---
+++ module3/3.1.ts
@@ -0,0 +1,21 @@
+{
+    // Class and object
+
+    class Animal {
+        constructor(public name:string, public sound: string){
+            this.name = name;
+            this.sound = sound;
+        }
+
+        makeSound(){
+            return `The ${this.name} says ${this.sound}`
+        }
+    }
+
+    const dog = new Animal('Dog', 'Ghew Ghew');
+    const cat = new Animal('Cat', 'Meaw');
+    const dogMakeSound = dog.makeSound();
+    const catMakeSound = cat.makeSound();
+    
+
+}
